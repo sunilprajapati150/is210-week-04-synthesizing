9b42e982b0487ed86edb41dad96d220d460efea4
Sunil Prajapati <nrk_619@yahoo.com>
2016-09-16 03:04:26 +0000

    Changes made

--- task_01.py
+++ task_01.py
@@ -2,14 +2,19 @@
 # -*- coding: utf-8 -*-
 """A good docstring"""
 
-ABSOLUTE_DIFFERENCE = 273.15
+ABSOLUTE_DIFFERENCE = int (273.15)
 import decimal
 
 def fahrenheit_to_celsius (degrees):
-    round(degrees,3)
-    f = ((degrees - 32)  * 5) / 9
-    print('Decimal(\'{0}\')'.format(f))
-    
+    round(degrees,2)
+    f = ((degrees - 32.0)  * 5.0) / 9.0
+    return 'Decimal(\'{0}\')'.format(f)
+
 def celsius_to_kelvin(degrees):
     k = degrees + ABSOLUTE_DIFFERENCE
-    print('Decimal(\'{0}\')'.format(k))
+    return 'Decimal(\'{0}\')'.format(k)
+
+def fahrenheit_to_kelvin(degrees):
+    a = fahrenheit_to_celsius(degrees)
+    b = celsius_to_kelvin(a)
+    return 'Decimal(\'{0}\')'.format(b)
